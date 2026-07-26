339544f0e48178f164530850e0bb280d8bce60dd
anrix <noreply@anrix.org>
2026-07-26 16:36:26 +0000

    Change shebang and simplify git remote setup

--- scripts/origin-setup.sh
+++ scripts/origin-setup.sh
@@ -1,10 +1,5 @@
-#!/bin/bash
+#!/bin/env bash
 rm -rf .git
 git init
 
-if git remote get-url origin > /dev/null 2>&1; then
-  git remote set-url origin https://github.com/accupara/los22 # Unnes code
-  git remote set-branches origin lineage-22.1 # Same
-  # Remove it
-else
-  git remote add -t lineage-22.1 origin https://github.com/accupara/los22
+git remote add -t lineage-22.1 origin https://github.com/accupara/los22 # Keep it
