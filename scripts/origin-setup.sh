b94e566d53b68ba7efc98fffc4b185ff7341a95e
anrix <noreply@anrix.org>
2026-07-26 16:15:21 +0000

    Update git remote URL to use branch lineage-22.1

--- scripts/origin-setup.sh
+++ scripts/origin-setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 git init
-git remote set-url origin https://github.com/accupara/los22 2>/dev/null || git remote add origin https://github.com/accupara/los22
+git remote set-url origin https://github.com/accupara/los22 -b lineage-22.1 2>/dev/null || git remote add origin https://github.com/accupara/los22 -b lineage-22.1
