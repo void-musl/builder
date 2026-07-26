f99b89d8cd2f6192226522e88326b5d056d1df78
anrix <noreply@anrix.org>
2026-07-26 16:32:54 +0000

    Reinitialize git repository and update remote settings
    Remove existing .git directory and reinitialize the repository with a new remote URL. Update remote settings for branches.

--- scripts/origin-setup.sh
+++ scripts/origin-setup.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+rm -rf .git
 git init
-git remote set-url origin https://github.com/accupara/los22 -b lineage-22.1 2>/dev/null || git remote add origin https://github.com/accupara/los22 -b lineage-22.1
+
+if git remote get-url origin > /dev/null 2>&1; then
+  git remote set-url origin https://github.com/accupara/los22 # Unnes code
+  git remote set-branches origin lineage-22.1 # Same
+  # Remove it
+else
+  git remote add -t lineage-22.1 origin https://github.com/accupara/los22
