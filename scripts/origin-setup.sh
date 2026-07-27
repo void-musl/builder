b6e687bbcb7d50c9f81d89b15e7c9c69be0bb7c5
anrix <noreply@anrix.org>
2026-07-27 16:43:54 +0000

    Add safe.directory configuration for git

--- scripts/origin-setup.sh
+++ scripts/origin-setup.sh
@@ -1,5 +1,6 @@
 #!/bin/env bash
+
+sudo git config --global --add safe.directory /crave-devspaces/actions-runner/_work/builder/builder/work_dir
 sudo rm -rf .git
 sudo git init
-
 sudo git remote add -t lineage-22.1 origin https://github.com/accupara/los22
