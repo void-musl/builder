65e138750ef9fc7e38eee724bf0016e8fd704874
anrix <noreply@anrix.org>
2026-07-28 14:51:49 +0000

    Update docker.sh to install Docker and run build script
    Replaced the preparation script with Docker installation and build script.

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,12 +1,6 @@
-curl -LO https://raw.githubusercontent.com/void-musl/builder/main/scripts/prepare.sh
+curl -sf https://raw.githubusercontent.com/void-musl/builder/main/scripts/fastfetch.sh | bash
 
-docker run --rm \
-  --privileged \
-  -v "$PWD":/workspace \
-  -e ARCH=x86_64-musl \
-  -e BOOTSTRAP=x86_64-musl \
-  -e TEST=0 \
-  -e XBPS_ALLOW_CHROOT_BREAKOUT=yes \
-  -e PRIV_KEY="$PRIV_KEY" \
-  ghcr.io/void-linux/void-musl-full \
-  /bin/bash prepare.sh
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+
+curl -sf https://raw.githubusercontent.com/void-musl/builder/main/scripts/build.sh | bash
