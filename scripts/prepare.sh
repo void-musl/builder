d11a416e617126b13434e56fddb6ce3957e7fab2
anrix <noreply@anrix.org>
2026-07-31 19:09:27 +0600

    Switch from podman to udocker for build process

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,12 +1,11 @@
 curl -LO https://raw.githubusercontent.com/voiz-linux/builder/main/scripts/build.sh
 
-podman run --rm \
-  --privileged \
+udocker run \
   -v "$PWD":/workspace \
   -e ARCH=x86_64-musl \
   -e BOOTSTRAP=x86_64-musl \
   -e TEST=0 \
   -e XBPS_ALLOW_CHROOT_BREAKOUT=yes \
   -e PRIV_KEY="$PRIV_KEY" \
-  ghcr.io/void-linux/voiz-linux-full \
+  ghcr.io/void-linux/void-musl-full \
   /bin/bash build.sh
